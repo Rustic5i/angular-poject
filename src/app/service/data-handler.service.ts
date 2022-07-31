05eb29904169b666675e9aa9e7982b6bb5862948
Руслан <rus5i1819@gmail.com>
2022-07-31 23:51:08 +0500

    Добавил компонент и сервис categories (статичные), выполнил декомпозицию HTML шаблона.

---
+++ src/app/service/data-handler.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root' //облость доступа будет начиная с корня
+})
+export class DataHandlerService {
+
+  constructor() { }
+}
